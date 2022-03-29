6825cba2b96fa6d0ebbf1de1aad4264af82c98d5
MishaelRatemo <ratemomishael@gmail.com>
2022-03-29 06:44:54 +0300

    password rest and change urls

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,20 @@
 from django.urls import  path
 from . import views
-
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/song', views.song_post, name='new_song'),
     path('profile',views.profile, name='profile'),
     path('profile/edit', views.edit_profile, name='edit-profile'),
+    path('change-password', views.change_password, name='change_password'),
+    path('reset-password', PasswordResetView.as_view(), name='reset_password'),
+    path('reset-password/done', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset-password/confrim', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
+    
+
+
     
     path('logout', views.logout, name='logout'),
     
